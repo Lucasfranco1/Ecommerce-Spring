c1f4d7f473a16a80ad95e11d19b52826c498e299
Lucas Franco <shananestudios@gmail.com>
2021-12-08 19:24:34 -0300

    funcionalidad para editar productos

--- src/main/java/com/ecommerce/controller/ProductoController.java
+++ src/main/java/com/ecommerce/controller/ProductoController.java
@@ -9,37 +9,55 @@ import com.ecommerce.model.Producto;
 import com.ecommerce.model.Usuario;
 import com.ecommerce.service.ProductoService;
 import java.util.List;
+import java.util.Optional;
 import java.util.logging.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
+
     @Autowired
     private ProductoService pS;
-    
+
     @GetMapping("")
-    public String show(ModelMap model){
-        List<Producto>todo=pS.findAll();
+    public String show(ModelMap model) {
+        List<Producto> todo = pS.findAll();
         model.addAttribute("productos", todo);
         return "productos/show";
     }
+
     @GetMapping("/create")
-    public String create(){
+    public String create() {
         return "productos/create";
     }
-    
+
     @PostMapping("/save")
-    public String save(Producto producto){
-        Usuario u=new Usuario(1, "", "", "", "", "", "", "");
+    public String save(Producto producto) {
+        Usuario u = new Usuario(1, "", "", "", "", "", "", "");
         producto.setUsuario(u);
         pS.save(producto);
         return "redirect:/productos";
     }
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, ModelMap model) {
+        Producto pr=new Producto();
+        Optional<Producto>respuesta=pS.get(id);
+        pr=respuesta.get();
+        model.addAttribute("productos", pr);
+        return "productos/edit";
+    }
+    @PostMapping("/update")
+    public String update(Producto producto){
+        pS.update(producto);
+        return "redirect:/productos";
+    }
+    
 }
